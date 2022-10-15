51177c9d6ea0ece91acc593c94cdac94619b4b81
emerson143 <emerson.143@hotmail.com>
2022-10-15 00:20:20 -0300

    conexão com o banco

--- crud-pessoa/crud-pessoa/Program.cs
+++ crud-pessoa/crud-pessoa/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using crud_pessoa.util;
+
 Console.WriteLine("Hello, World!");
+
+
+Conexao.connect();
---
+++ crud-pessoa/crud-pessoa/util/Conexao.cs
@@ -0,0 +1,44 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_pessoa.util
+{
+    public class Conexao
+     {
+        
+        public static void connect()
+        {
+            string server = "localhost";
+            string database = "c#_projeto";
+            string user = "root";
+            string password = "";
+            string port = "3306";
+            string sslM = "none";
+
+            string connString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}; SslMode={5}", 
+                server, port, user, password, database, sslM);
+
+            var conn = new MySqlConnection(connString);
+            try
+            {
+                conn.Open();
+
+                Console.WriteLine("Connection Successful");
+
+                conn.Close();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message + connString);
+            }
+
+        }
+
+        
+    }
+}
+
